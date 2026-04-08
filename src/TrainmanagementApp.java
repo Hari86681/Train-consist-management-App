6be247c4ba67053c024c46fbc26441866b9c45bb
Hari86681 <muhariharan333@gmail.com>
2026-04-08 15:32:02 +0530

    UC17:Sort Bogie Names

--- src/TrainmanagementApp.java
+++ src/TrainmanagementApp.java
@@ -1,24 +1,23 @@
+import java.util.Arrays;
+
 public class TrainmanagementApp {
+
     public static void main(String[] args) {
-        int[] capacities = {60, 40, 80, 50, 70};
-        System.out.println("Before Sorting:");
-        printArray(capacities);
-        for (int i = 0; i < capacities.length - 1; i++) {
-            for (int j = 0; j < capacities.length - 1 - i; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-        System.out.println("\nAfter Sorting:");
-        printArray(capacities);
-    }
-    public static void printArray(int[] arr) {
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
+
+        System.out.println("===============================================");
+        System.out.println("UC17 - Sort Bogie Names Using Arrays.sort()");
+        System.out.println("===============================================\n");
+
+        String[] bogies = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
+
+        System.out.println("Original Bogie Names:");
+        System.out.println(Arrays.toString(bogies));
+
+        Arrays.sort(bogies);
+
+        System.out.println("\nSorted Bogie Names (Alphabetical):");
+        System.out.println(Arrays.toString(bogies));
+
+        System.out.println("\nUC17 sorting completed...");
     }
 }
